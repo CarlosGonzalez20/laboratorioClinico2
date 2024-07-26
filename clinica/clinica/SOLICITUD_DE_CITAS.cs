03c54520fc3dc603af767980fbf20a1112090d6b
502-Alexander! <mcrisostomoa@miumg.edu.gt>
2024-07-26 14:47:01 -0600

    Se realizaron las formas Cita, Muestra y tipo de muestra, se unifico en uno solo la forma de Muestra y tipo de muestra.

---
+++ clinica/clinica/SOLICITUD_DE_CITAS.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+
+namespace clinica
+{
+    public partial class SOLICITUD_DE_CITAS : MaterialSkin.Controls.MaterialForm
+    {
+        public SOLICITUD_DE_CITAS()
+        {
+            InitializeComponent();
+        }
+
+        private void SOLICITUD_DE_CITAS_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
